b6df87f38bf513de384d4ed928a2ecd2c34fa351
Srinivasa Dodda <srinivasa.dodda@outlook.com>
2023-05-06 23:40:25 +0530

    Skeleton

---
+++ src/main/java/com/vamsee/explorespringbootmongo/ExploreSpringBootMongoApplication.java
@@ -0,0 +1,13 @@
+package com.vamsee.explorespringbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreSpringBootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreSpringBootMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vamsee/explorespringbootmongo/ExploreSpringBootMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamsee.explorespringbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreSpringBootMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
